ceff37fd9d039c685e8b7844e23063d24291ebd9
bitbeast18 <pro.bbcom18@gmail.com>
2020-02-11 19:06:54 +0530

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build 
+
+cd dist
+
+git init 
+git add -A
+git commit -m 'deployment'
+
+git push -f git@github.com:bitbeast18/sandbox-ui.git master:gh-pages
+
+cd -
