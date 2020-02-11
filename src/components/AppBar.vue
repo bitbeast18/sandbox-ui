d443c18ea0774f9efab4675cf8e737b04ffe2584
bitbeast18 <pro.bbcom18@gmail.com>
2020-02-11 18:31:42 +0530

    Added home button to appbar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -2,6 +2,7 @@
     <v-app-bar app>
         <span class="headline font-weight-bold"> Sandbox UI </span>
         <v-spacer></v-spacer>
-        <v-btn large outlined rounded> Contact Us </v-btn>
+        <v-btn class="mr-4" large outlined rounded router to="/"> Home </v-btn>
+        <v-btn class="mr-4" large outlined rounded> Contact Us </v-btn>
     </v-app-bar>
 </template>
