00b3dbb3e5975fc0f4a04716f317cd91e315c231
bitbeast18 <pro.bbcom18@gmail.com>
2020-02-13 18:24:49 +0530

    deployment

---
+++ precache-manifest.8943a7130f13c519e17b845655adfbe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817bfc1141326b08b7b3",
+    "url": "/sandbox-ui/css/chunk-vendors.f3a1d1d5.css"
+  },
+  {
+    "revision": "baaf186a72f30550a70c8d8b737c858c",
+    "url": "/sandbox-ui/index.html"
+  },
+  {
+    "revision": "ad187fe716887de9ae1b",
+    "url": "/sandbox-ui/js/app.9fd366ed.js"
+  },
+  {
+    "revision": "817bfc1141326b08b7b3",
+    "url": "/sandbox-ui/js/chunk-vendors.180850d0.js"
+  },
+  {
+    "revision": "c2eda35a25f1fabd5dfde424f8a38832",
+    "url": "/sandbox-ui/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sandbox-ui/robots.txt"
+  }
+]);
